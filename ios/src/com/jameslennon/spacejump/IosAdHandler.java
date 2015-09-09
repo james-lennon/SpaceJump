ecfb8d583172992269a3e137d9fa66d39275373b
James Lennon <jameslennon321@gmail.com>
2015-09-08 23:15:40 -0400

    Disabled ads, added preview video

--- ios/src/com/jameslennon/spacejump/IosAdHandler.java
+++ ios/src/com/jameslennon/spacejump/IosAdHandler.java
@@ -10,13 +10,13 @@ public class IosAdHandler extends AdHandler {
 
     @Override
     public void init() {
-        Chartboost.start("554fc70f04b01648b4d247df", "547428f67014890a6a5d7133321a765d0e763e6a", new ChartboostDelegateAdapter(){
+//        Chartboost.start("554fc70f04b01648b4d247df", "547428f67014890a6a5d7133321a765d0e763e6a", new ChartboostDelegateAdapter(){
 
-        });
+//        });
     }
 
     @Override
     public void show() {
-        Chartboost.showInterstitial("Game Over");
+//        Chartboost.showInterstitial("Game Over");
     }
 }
